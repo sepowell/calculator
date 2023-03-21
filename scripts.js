14a3dbea1a649eae83270ede73a88d73019c961b
sepowell <seanp1998@gmail.com>
2023-03-20 22:19:21 -0700

    Add addition button second screen push
    Once addition button is pressed this will push the history of pressed keys to second screen above output screen.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,16 @@
 let mainOutput = document.querySelector(".main-output");
+let secondOutput = document.querySelector(".secondary-output");
 let clearButton = document.querySelector(".btn-clear");
 let deleteButton = document.querySelector(".btn-delete");
 let numberButton = document.querySelectorAll(".btn-num");
+let equalsButton = document.querySelector(".btn-equals");
+let addButton = document.querySelector(".btn-add");
 
 clearButton.addEventListener('click', clear);
 deleteButton.addEventListener('click', deleteNumber);
+// equalsButton.addEventListener('click', evaluate);
+addButton.addEventListener('click', add);
+
 
 numberButton.forEach(button => {
   button.addEventListener("click", () => {
@@ -23,3 +29,8 @@ function clear() {
 function deleteNumber() {
   mainOutput.textContent = mainOutput.textContent.toString().slice(0, -1);
 }
+
+function add() {
+  secondOutput.textContent = mainOutput.textContent + ' ' + '+';
+}
+
