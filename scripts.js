f3ae398d5a3caf7d9c618a3521390c37a3c7d96a
sepowell <seanp1998@gmail.com>
2023-03-20 21:41:28 -0700

    Add clear button ability

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 let numberButton = document.querySelectorAll(".btn-num");
+let clearButton = document.querySelector(".btn-clear");
 let mainOutput = document.querySelector(".main-output");
+
+clearButton.addEventListener('click', clear);
 
 numberButton.forEach(button => {
   button.addEventListener("click", () => {
@@ -9,4 +12,8 @@ numberButton.forEach(button => {
 
 function appendNumber(number) {
   mainOutput.textContent += number;
+}
+
+function clear() {
+  mainOutput.textContent = 0;
 }
